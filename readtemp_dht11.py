3da580c756893fa67e08b9ec8d0743933c713475
Niklas Bergelv <niklas@bergelv.se>
2023-04-11 16:13:01 +0200

    first update, possible to execute

--- readtemp_dht11.py
+++ readtemp_dht11.py
@@ -1,10 +1,71 @@
 #!/usr/bin/env python3
 
-import board
-import adafruit_dht
+# import board
+# import adafruit_dht
+import random
+import logging
+import time
 
-readValues():
-    dhtDevice = adafruit_dht.DHT11(board.D18)
+MAX_NUMBER_OF_FAILED_READINGS = 100
+SLEEP_BETWEEN_READINGS = 2
+
+def getrandomvalue(min, max):
+    logging.debug(f"getrandomvalue, min:{min}, max:{max}")
+    return random.uniform(min, max)
+
+def readvalues(number_of_readings=1):
+    logging.debug(f"readvalues, number of readings: {number_of_readings}")
+    avg_temp = None
+    avg_hum = None
     try:
-        
-    return (temp, hum)
+        # dhtDevice = adafruit_dht.DHT11(board.D18)
+        failed_readings = 0
+        readings = []
+        while True:
+            try:
+                temp = getrandomvalue(15, 24)  # dhtDevice.temperature
+                hum = getrandomvalue(29,33)  # dhtDevice.humidity
+                logging.debug(f"read values, temperature:{temp}, humidity:{hum}")
+                if (temp is not None) and (hum is not None):
+                    readings.append((temp, hum))
+                    failed_readings = 0
+                    if len(readings) == number_of_readings:
+                        logging.debug(f"Number of succesful readings: {len(readings)}, time to break")
+                        break
+                else:
+                    failed_readings += 1
+                    if failed_readings > MAX_NUMBER_OF_FAILED_READINGS:
+                        raise Exception(f"Number of failed readings above MAX_NUMBER_OF_FAILED_READINGS: "
+                                        f"{MAX_NUMBER_OF_FAILED_READINGS}")
+
+                time.sleep(SLEEP_BETWEEN_READINGS)
+
+            except RuntimeError as error:
+                # Errors happens fairly often, DHT's are hard to read, just keep going
+                logging.error(error.args[0])
+                time.sleep(SLEEP_BETWEEN_READINGS)
+                continue
+
+        xtotal = 0
+        ytotal = 0
+        for x, y in readings:
+            xtotal += x
+            ytotal += y
+
+        avg_temp = xtotal / len(readings)
+        avg_hum = ytotal / len(readings)
+
+    except:
+        logging.error("An exception occurred")
+
+    logging.info(f"average readings returned, temperature:{avg_temp}, humidity:{avg_hum}")
+
+    return avg_temp, avg_hum
+
+def main():
+    logging.basicConfig(filename="readtemp_dht11.log", filemode="w", level=logging.DEBUG, format='%(asctime)s %(message)s')
+    temperature, humidity = readvalues(10)
+    print(f"temperature: {temperature}, humidity: {humidity}")
+
+if __name__ == "__main__":
+    main()
